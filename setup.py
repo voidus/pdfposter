549c2cf74aecb6f75321b4d32504386f1473160d
Hartmut Goebel <h.goebel@goebel-consult.de>
2008-04-28 12:41:42 +0000

    Bumping version number to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pdfposter",
-    version = "0.1",
+    version = "0.2",
     scripts = ['pdfposter'],
     install_requires = ['pyPdf>1.10'],
 
