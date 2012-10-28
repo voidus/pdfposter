4d87b205238bdc20153d4cb6a0b3f30d52c69559
Hartmut Goebel <h.goebel@crazy-compilers.com>
2012-10-28 21:49:39 +0100

    Add more test-cases for box-specifications.

--- test/test_box.py
+++ test/test_box.py
@@ -40,6 +40,30 @@ class TextBoxes(unittest.TestCase):
     def test_papersize_abbreviation(self):
         box = self._parse_box('1x1let')
         self.assertBoxValue(box, 'unit', 'letter')
+        box = self._parse_box('1x1envdin')
+        self.assertBoxValue(box, 'unit', 'envdinlang')
+
+    def test_ambiguous_papersize_abbreviation_raises(self):
+        self.assertRaisesRegexp(
+            ArgParserError, "papersize name .* is not unique",
+            self._parse_box, '1x1a')
+
+    def test_unknown_papersize_raises(self):
+        self.assertRaisesRegexp(
+            ArgParserError, "I don't understand your papersize name",
+            self._parse_box, '1x2yyy')
+        self.assertRaisesRegexp(
+            ArgParserError, "I don't understand your papersize name",
+            self._parse_box, '1x2xxx')
+
+    def test_wrong_box_definition_raises(self):
+        self.assertRaisesRegexp(
+            ArgParserError, "I don't understand your box specification",
+            self._parse_box, '2yyy')
+        self.assertRaisesRegexp(
+            ArgParserError, "I don't understand your box specification",
+            self._parse_box, '2xxx')
+
 
 if __name__ == '__main__':
     unittest.main()
